47e4cd40fc7ef6402fda4e3234874844e5ee14af
lrachadel <lucas.rachadel13@gmail.com>
2025-12-13 00:38:12 -0300

    Create data, rules and seed for api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config.service';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule, TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
