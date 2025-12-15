19de7a47fce4cb07a8a25f7bb07dc172a0e2c07b
lrachadel <lucas.rachadel13@gmail.com>
2025-12-14 22:58:23 -0300

    adjust vercel deploy in frontend

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone', // Uncomment for Docker deployment
 };
 
 export default nextConfig;
